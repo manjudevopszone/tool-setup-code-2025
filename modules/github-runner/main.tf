c2f8c71d8cdece92f51813152a04b8304291c6e4
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2026-03-12 19:45:22 +0530

    helm deployment

--- modules/github-runner/main.tf
+++ modules/github-runner/main.tf
@@ -13,7 +13,7 @@ resource "aws_route53_record" "records" {
   name    = "${var.tag_name}-internal"
   type    = "A"
   ttl     = 30
-  records = [aws_instance.tool.private_ip]
+  records = [aws_instance.tool.public_ip]
 }
 
 
